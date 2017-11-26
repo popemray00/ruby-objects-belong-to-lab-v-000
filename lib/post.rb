f8592254b265311fc984adeb98cd12cd292f9884
Michael Pope <popemray1986@gmail.com>
2017-11-26 21:19:07 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,9 @@
+class Post
+  attr_accessor :title, :author
+
+  def inititlize
+    @title = title
+    @author = author
+  end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,9 @@
+class Song
+  attr_accessor :title, :artist
+
+  def inititlize
+    @title = title
+    @author = artist
+  end
+
+end
